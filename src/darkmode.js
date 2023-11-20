0bf7936fc5f0d3533999c27b98e2e0dc0f9fdebd
fdiazTAG <fdiaz@amherst.com>
2023-11-20 11:10:01 -0300

    fixes

--- src/darkmode.js
+++ src/darkmode.js
@@ -1,5 +1,17 @@
 function darkModeListener() {
+    const darkModeToggle = document.querySelector("#dark-toggle");
+    if(!darkModeToggle.checked){
+        sessionStorage.setItem('dark', '');
+    }
+    else{
+        sessionStorage.setItem('dark', 'dark');
+    }
     document.querySelector("html").classList.toggle("dark");
 }
 
 document.querySelector("input[type='checkbox']#dark-toggle").addEventListener("click", darkModeListener);
+const storedTheme = sessionStorage.getItem('dark');
+if(storedTheme === 'dark') {
+    document.querySelector("#dark-toggle").checked = 1;
+    document.querySelector("html").classList.add("dark");
+}
